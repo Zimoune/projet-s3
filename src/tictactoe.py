06b125704366a3c72284a854dfd8b3c422ad1520
Lefebvre Ryan <lefebvrer@bad.fil.univ-lille1.fr>
2015-11-23 14:50:23 +0100

    correction initsituation

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -11,6 +11,10 @@ game = Game.make_game()
 
 ######################
 
+
+
+##### Selectors #####
+
 def get_player1(game):
     return game['player1']
 
@@ -29,11 +33,19 @@ def get_color(situation, x, y):
 def set_color(situation, x, y, color):
     situation[x][y]['color'] = color
 
+def set_player1(player):
+    game['player1'] = player
+
+def set_player2(player):
+    game['player2'] = player
+
 def get_position(situation, x, y):
     return situation[x][y]['position']
 
 def get_grid_color(game, x, y):
     return game['grid'][x][y]['color']
+
+########################
 
 def initSituation(game_name):
     """builds the initial situation for the game. 
@@ -43,14 +55,96 @@ def initSituation(game_name):
     :returns: *(situation)* the situation at the beginning of the game
     """
     try:
-        get_player1(game) == Player.create(input("name player 1: "), "cross")
-        get_player2(game) == Player.create(input("name player 2: "), "circle")
+        set_player1(Player.create(input("name player 1: "), "cross"))
+        set_player2(Player.create(input("name player 2: "), "circle"))
     except KeyboardInterrupt:
         raise KeyboardInterrupt
     except:
         initSituation(game)
     
     return get_grid(game)
+
+
+
+def nextSituations(game, situation, player):
+    """
+    returns the list of situations that can be reached from given situation by the player in the game
+
+    :param game: the game
+    :type game: a two players game
+    :param situation: the current situation
+    :type situation: a game situation
+    :param player: the current player
+    :type player: player
+    :returns: *(list<situtation>)* -- the list of situations that can be reached from given situation when player plays one round in the game
+    """
+    l_situations = []
+    for x in range(3):
+        for y in range(3):
+            if get_color(situation, x, y) == None:
+                l_situations += [get_position(situation, x, y)]
+    return l_situations
+    
+
+
+
+def getWinner(game, situation, player):
+    """
+    Gives the winner of the game that end in situation
+
+    :param game: the game 
+    :type game: game
+    :param situation: the situation which is a final game situation
+    :type situation: a game situation
+    :param player: the player who should have played if situation was not final (then other player plays previous turn)
+    :type player: player
+    :returns: *(player)* -- the winner player or None in case of tie game
+
+    :CU: situation is a final situation
+    """
+    
+    if get_nb_plays(game) == 9:
+        return None
+    else:
+        return player
+
+
+
+
+
+def displaySituation(situation):
+    """
+    displays the situation
+
+    :param situation: the situation to display
+    :type situation: a game situation
+    """
+
+    pass
+
+def humanPlayerPlays(game, player, situation):
+    """
+    makes the human player plays for given situation in the game
+
+    :param game: the game 
+    :type game: game
+    :param player: the human player
+    :type player: player
+    :param situation: the current situation
+    :type situation: a game situation
+    :returns: *(game situtation)* -- the game situation reached afte the human player play
+    """
+    coord = input("Where would you play? x, y ")
+    try:
+        x,y = coord.split(',')
+        set_color(situation, int(x), int(y), Player.get_color(player))
+        return game
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
+    except:
+        print("input must be 2 seperated with a coma x,y . (x = width , y = height)")
+        humanPlayerPlays(game,player,situation)
+
 
 
 
@@ -112,86 +206,6 @@ def playerCanPlay(game, situation, player):
                     return False
     return False
 
-
-def nextSituations(game, situation, player):
-    """
-    returns the list of situations that can be reached from given situation by the player in the game
-
-    :param game: the game
-    :type game: a two players game
-    :param situation: the current situation
-    :type situation: a game situation
-    :param player: the current player
-    :type player: player
-    :returns: *(list<situtation>)* -- the list of situations that can be reached from given situation when player plays one round in the game
-    """
-    l_situations = []
-    for x in range(3):
-        for y in range(3):
-            if get_color(situation, x, y) == None:
-                l_situations += [get_position(situation, x, y)]
-    return l_situations
-    
-
-
-
-def getWinner(game, situation, player):
-    """
-    Gives the winner of the game that end in situation
-
-    :param game: the game 
-    :type game: game
-    :param situation: the situation which is a final game situation
-    :type situation: a game situation
-    :param player: the player who should have played if situation was not final (then other player plays previous turn)
-    :type player: player
-    :returns: *(player)* -- the winner player or None in case of tie game
-
-    :CU: situation is a final situation
-    """
-    
-    if get_nb_plays(game) == 9:
-        return None
-    else:
-        return player
-
-
-
-
-
-def displaySituation(situation):
-    """
-    displays the situation
-
-    :param situation: the situation to display
-    :type situation: a game situation
-    """
-
-    get_grid(game) = situation
-
-
-def humanPlayerPlays(game, player, situation):
-    """
-    makes the human player plays for given situation in the game
-
-    :param game: the game 
-    :type game: game
-    :param player: the human player
-    :type player: player
-    :param situation: the current situation
-    :type situation: a game situation
-    :returns: *(game situtation)* -- the game situation reached afte the human player play
-    """
-    coord = input("Where would you play? x, y ")
-    try:
-        x,y = coord.split(',')
-        set_color(situation, int(x), int(y), Player.get_color(player))
-        return game
-    except KeyboardInterrupt:
-        raise KeyboardInterrupt
-    except:
-        print("input must be 2 seperated with a coma x,y . (x = width , y = height)")
-        humanPlayerPlays(game,player,situation)
 
 
             
