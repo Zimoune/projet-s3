30075b3408e5f1d7d1463fa8f924986ab36086b2
Zimoune <lepallac.simon@gmail.com>
2015-11-23 14:59:54 +0100

    Debut du twoplayergame

--- src/twoplayergame.py
+++ src/twoplayergame.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import abstractgame as Game
+import tictactoe as Game
+import player
+
+currentPlayer = None
 
 def play():
-    raise NotImplemented
+    global currentPlayer
+    situation = Game.initSituation("tictactoe")
+    if currentPlayer == None:
+        currentPlayer = Game.get_player1(Game.game)
+    elif currentPlayer == Game.get_player1(Game.game):
+        currentPlayer = Game.get_player2(Game.game)
+    print(player.get_name(currentPlayer)," turn")
+    #while not Game.isFinished(Game.game):   
+        
+    
+if __name__ == '__main__':
+    play()
