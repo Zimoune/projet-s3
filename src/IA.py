8ec2d78a40d55bf4452249e302ee35de1602aa50
ElProton <lefebvreryan@hotmail.com>
2015-11-25 20:10:34 +0100

    eval nim & minmax & documentation

--- src/IA.py
+++ src/IA.py
@@ -1,23 +1,14 @@
-import tictactoe as Tictactoe
 
-def tictactoeEval(situation, player):
-    pass
-
-def TictactoeCoef(player):
-    if player == Tictactoe.get_player1(Tictactoe.game):
-        return -1
-    else:
-        return 1
 
 def min_max1(situation , player):
     if Tictactoe.isFinished(situation):
-        return (evalu(situation , player)*coef(player) , situation)
+        return (tictactoeEval(situation , player)*coef(player) , situation)
     else:
-        nextSituations = Tictactoe.nextSituations(Tictactoe.game , situation , player)
+        nextSituations = Tictactoe.nextSituations(game , situation , player)
         if coef(player) == 1:
             return max(min_max(nextSit, Tictactoe.get_player1(Tictactoe.game)) for nextSit in nextSituations)
         else:
-            return min(min_max(nextSit, Tictactoe.get_player1(Tictactoe.game)) for nextSit in nextSituations)
+            return min(min_max(nextSit, Tictactoe.get_player2(Tictactoe.game)) for nextSit in nextSituations)
             
 
 
