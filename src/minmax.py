d6e8d3d939525d585192bfbf0dd270cad586c656
ElProton <lefebvreryan@hotmail.com>
2015-12-06 18:20:06 +0100

    modif minmax

--- src/minmax.py
+++ src/minmax.py
@@ -1,16 +1,54 @@
-import tictactoe as Game
+
 
 def min_max1(situation, player):
+    game_name = "nim"
+
+    if game_name == "nim":
+        import nim_game as Game
+        mod = __import__("nim_game")
+
+    elif game_name == "othello":
+        import othello as Game
+        mod = __import__("othello")
+
+    else:
+        import tictactoe as Game
+        mod = __import__("tictactoe")
     if Game.isFinished(situation):
-        print(situation)
         score = Game.evalFunction(situation , player)*Game.coef(player)
+        print(situation)
         return (score , situation)
     else:
         nextSituations = Game.nextSituations(situation, player)
-        print(nextSituations)
-        if Game.:
-            return max([(min_max1(nextSit, Game.get_player1(Game.game))[0], nextSit) for nextSit in nextSituations])
+        if Game.coef(player) == 1:
+            return max([(min_max1(nextSit, Game.get_inv_player(player))[1], nextSit) for nextSit in nextSituations])
         else:
 
-            return min([(min_max1(nextSit, Game.get_player2(Game.game))[1], nextSit) for nextSit in nextSituations])
+            return min([(min_max1(nextSit, Game.get_inv_player(player))[1], nextSit) for nextSit in nextSituations])
+            
+def min_max2(situation, player, depht):
+    game_name = "nim"
+
+    if game_name == "nim":
+        import nim_game as Game
+        mod = __import__("nim_game")
+
+    elif game_name == "othello":
+        import othello as Game
+        mod = __import__("othello")
+
+    else:
+        import tictactoe as Game
+        mod = __import__("tictactoe")
+    if Game.isFinished(situation) or depht == 0:
+        score = Game.evalFunction(situation , player)*Game.coef(player)
+        print(situation)
+        return (score , situation)
+    else:
+        nextSituations = Game.nextSituations(situation, player)
+        if Game.coef(player) == 1:
+            return max([(min_max1(nextSit, Game.get_inv_player(player), depht -1)[1], nextSit) for nextSit in nextSituations])
+        else:
+
+            return min([(min_max1(nextSit, Game.get_inv_player(player))[1], nextSit) for nextSit in nextSituations])
             
